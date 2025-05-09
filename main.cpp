8430da3a9a75e122298fb34f2a03194dd5bc8f13
MaksimCpp <maximus.bychkov@gmail.com>
2025-05-09 16:10:50 +0300

    erase

--- main.cpp
+++ main.cpp
@@ -12,5 +12,39 @@ void print(std::vector<T> v) {
 }
 
 int main() {
-   
+    SquareMatrix a(1), b(2), c(3);
+
+    std::cout << "Enter matrix a:\n";
+    std::cin >> a;
+    std::cout << "\n";
+    std::cout << a;
+    std::cout << "\n";
+
+    std::cout << "Enter matrix b:\n";
+    std::cin >> b;
+    std::cout << "\n";
+    std::cout << b;
+    std::cout << "\n";
+
+    std::cout << "Enter matrix c:\n";
+    std::cin >> c;
+    std::cout << "\n";
+    std::cout << c;
+    std::cout << "\n";
+
+    std::vector<double> matrixVector = {a.determinant(), b.determinant(), c.determinant()};
+    std::cout << "Before erase:\n";
+    print(matrixVector);
+
+    auto start = std::remove_if(
+        matrixVector.begin(),
+        matrixVector.end(),
+        [](int element) {
+            return element % 2 == 0;
+        }
+    );
+
+    matrixVector.erase(start, matrixVector.end());
+    std::cout << "\nAfter erase:\n";
+    print(matrixVector);
 }
